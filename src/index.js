b5e394a237fea00266dc3cf343e2ad2d182ce4ac
Darios Petrov <darios.petrov93@gmail.com>
2023-02-01 12:30:23 +0200

    Add express-handlebars

--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
 const express = require('express');
 
 const config = require('./config');
+const setupViewEngine = require('./config/viewEngine');
 
-const app = express()
+const app = express();
+setupViewEngine(app);
 
 
-app.get('/', (req, res) =>{
-    res.send('Home page');
+app.get('/', (req, res) => {
+    res.render('home');
 });
 
 
-app.listen(config.PORT, () => console.log(`Server is running  on ${config.PORT}...`));
+app.listen(config.PORT, () => console.log(`Server is running on ${config.PORT}...`));
